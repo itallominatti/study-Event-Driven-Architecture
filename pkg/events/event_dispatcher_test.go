9dfb71b27ed26f067997dc78c06e7f71b7a9aed5
itallominatti <itallo.monteiro@propig.com.br>
2025-10-17 08:17:43 -0300

    feat: add event dispatcher and related interfaces

---
+++ pkg/events/event_dispatcher_test.go
@@ -0,0 +1,26 @@
+package events
+
+import "time"
+
+type TestEvent struct {
+	Name    string
+	Payload interface{}
+}
+
+func (e *TestEvent) GetName() string {
+	return e.Name
+}
+
+func (e *TestEvent) GetPayload() interface{} {
+	return e.Payload
+}
+
+func (e *TestEvent) GetDateTime() time.Time {
+	return time.Now()
+}
+
+type TestEventHandler struct {
+	ID int
+}
+
+func (h *TestEventHandler) Handle(event EventInterface) {}
