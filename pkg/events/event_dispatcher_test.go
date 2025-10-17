ca73c20bc6c0acdd686c1d2f6ea02a1101e57860
itallominatti <itallo.monteiro@propig.com.br>
2025-10-17 08:28:59 -0300

    feat: update go.mod and go.sum for dependencies; add event dispatcher test suite

--- pkg/events/event_dispatcher_test.go
+++ pkg/events/event_dispatcher_test.go
@@ -1,6 +1,12 @@
 package events
 
-import "time"
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+)
 
 type TestEvent struct {
 	Name    string
@@ -24,3 +30,30 @@ type TestEventHandler struct {
 }
 
 func (h *TestEventHandler) Handle(event EventInterface) {}
+
+type EventDispatcherTestSuite struct {
+	suite.Suite
+	event           TestEvent
+	event2          TestEvent
+	handler         TestEventHandler
+	handler2        TestEventHandler
+	handler3        TestEventHandler
+	eventDispatcher *EventDispatcher
+}
+
+func (suite *EventDispatcherTestSuite) SetupTest() {
+	suite.eventDispatcher = NewEventDispatcher()
+	suite.handler = TestEventHandler{}
+	suite.handler2 = TestEventHandler{}
+	suite.handler3 = TestEventHandler{}
+	suite.event = TestEvent{Name: "TestEvent", Payload: "Event Payload"}
+	suite.event2 = TestEvent{Name: "TestEvent2", Payload: "Event2 Payload"}
+}
+
+func (suite *EventDispatcherTestSuite) TestEventDispatcher_Register() {
+	assert.True(suite.T(), true)
+}
+
+func TestSuite(t *testing.T) {
+	suite.Run(t, new(EventDispatcherTestSuite))
+}
